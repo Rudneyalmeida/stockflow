aa834e0f2c746f0b02811a0edda883fa7502c0df
Henrique Waxman <henriquewsouza@gmail.com>
2023-06-06 12:28:42 -0300

    parcial addproduct

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,17 +6,30 @@ class ProductsController < ApplicationController
   end
 
   def new
-  end
-
-  def edit
+    @product = Product.new
+    @product.stocks.build
   end
 
   def create
+    @product = Product.new(product_params)
+  
+    if @product.save
+      redirect_to @product
+    else
+      render :new
+    end
+  end
+
+  def edit
   end
 
   def update
   end
 
   def destroy
+  end
+
+  def product_params
+    params.require(:product).permit(:category, :name, :description, :expiration, :location, stocks_attributes: [:quantity, :user_id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  has_many :stocks
+  has_many :products, through: :stocks
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
 end
