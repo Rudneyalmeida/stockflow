dcd5485a05f6d9e49eab1a3717330cd1798c93e4
Rudney Santos <rudney.a.s@hotmail.com>
2023-06-06 15:13:38 -0300

    inicio do front

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,24 +1,32 @@
 class ProductsController < ApplicationController
-
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
   def index
-   
   end
-
   def show
   end
-
   def new
+    @product = Product.new
+    @product.stocks.build
   end
-
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new
+    end
+  end
   def edit
   end
-
-  def create
-  end
-
   def update
   end
-
   def destroy
+  end
+  private
+  def product_params
+    params.require(:product).permit(:category, :name, :description, :expiration, :location, :photo, stocks_attributes: [:quantity, :user_id])
+  end
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
