90277a418d860c88811b5b82b5ac16934febd254
fernanda suzukawa <fernandasuzukawa@hotmail.com>
2023-06-06 14:12:22 -0300

    fdsf

--- app/controllers/user_controller.rb
+++
@@ -1,5 +0,0 @@
-class UserController < ApplicationController
-  def show
-    @user = User.find_by_id(params[:id])
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :products
   devise_for :users
   root to: "pages#home"
-  resources :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
