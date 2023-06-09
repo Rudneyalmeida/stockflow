2085e44b2117219e086ba2d2ee2ad14e87a63c65
fernanda suzukawa <fernandasuzukawa@hotmail.com>
2023-06-09 12:15:36 -0300

    navbar unless

---
+++ app/helpers/render_pages_helper.rb
@@ -0,0 +1,5 @@
+module RenderPagesHelper
+  def hide_navbar?
+    current_page?(new_user_session_path) || current_page?(new_user_registration_path) || current_page?(new_user_password_path) || current_page?(edit_user_password_path) || current_page?(cancel_user_registration_path) || current_page?(edit_user_registration_path)
+  end
+end
