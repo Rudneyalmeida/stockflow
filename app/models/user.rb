654215ad0f6f4193c92f451635024a5ba8e177df
Kaio Lemos <kaiolemos1@gmail.com>
2023-06-11 15:45:38 -0300

    notification

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
-
-  has_many :offers, dependent: :destroy
   has_many :products, dependent: :destroy
+  has_many :offers, dependent: :destroy
+  has_many :offers_received, through: :products, source: :offers
   has_one_attached :photo
+  has_many :notifications
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
