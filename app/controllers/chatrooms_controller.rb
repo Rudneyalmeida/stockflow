406f9f4bbcc884b840c99e4a27ea7d9c0b15ff4d
fernanda suzukawa <fernandasuzukawa@hotmail.com>
2023-06-13 15:58:23 -0300

    chatroom backend complete

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,22 +1,25 @@
 class ChatroomsController < ApplicationController
   def index
-    @chatrooms = current_user.chatrooms
+    @chatrooms = current_user.chatrooms + Chatroom.where("user_two_id = #{current_user.id}")
   end
 
   def show
     @chatroom = Chatroom.find(params[:id])
     @chatrooms = Chatroom.all
     @message = Message.new
+    @chatrooms = current_user.chatrooms + Chatroom.where("user_two_id = #{current_user.id}")
   end
 
   def create
     @product = Product.find(params[:product_id])
     @user = current_user
     @user2 = @product.user
-    @chatroom = Chatroom.create(user: current_user, user_two: @product.user)
-    @chatroom.user = @user
-    @chatroom.user_two = @user2
-    @chatroom.name = "Messaging #{@user2.name}"
+    @chatroom = Chatroom.create(user: @user, user_two: @user2)
+    @chatroom.user.name = @user.name
+    @chatroom.user_two.name = @user2.name
+    @chatroom.user_two_id = @user2.id
+    @chatroom.name2 = @user.name
+    @chatroom.name = @user2.name
     if @chatroom.save
       redirect_to chatroom_path(@chatroom)
     else
